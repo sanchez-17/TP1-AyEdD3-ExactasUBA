5f5835e68c8df099dd49c1ec6cd02ea63af4a322
gaston sanchez <sanchez-17@outlook.com>
2023-09-07 18:36:24 -0300

    aaaa

--- TP1 Tecnicas Algoritmicas/Ej.3 Saldos Sospechosos/saldos.cpp
+++ TP1 Tecnicas Algoritmicas/Ej.3 Saldos Sospechosos/saldos.cpp
@@ -6,15 +6,27 @@
 int m, N, W, RANGO; vector<int> libro;vector<char> RES; int sumaMax, sumaMin;
 vector<vector<int>> memo ;
 bool f(int i,int sum){
-    if(i == 0) return sum == 0;
+    if(i == -1) return sum == 0;
     else{
-        if(memo[i][sum + RANGO] != -1) return memo[i][sum + RANGO];
+        //DBG3(RES,i,sum);
+        //cout << "aca hay un " << memo[i][sum + RANGO] << "\n";
+        //DBG(memo);
+        //DBG(memo[i][sum + sumaMax]);
+        //DBGY(i);DBG4(sumaMax,sum,sumaMax + sum, RANGO);
+        if(memo[i][sum + sumaMax] != -1) return memo[i][sum + sumaMax];
+        //cout << "llegue lol";
+        bool res;
+        RES.pb('+');
         bool a = f(i-1, sum-libro[i]);
+        RES.ppb();
+        RES.pb('-');
         bool b = f(i-1, sum+libro[i]);
-        if( a && b ) RES.pb('?');
-        else if( a ) RES.pb('-');
-        else RES.pb('+');
-        return a || b;
+        RES.ppb();
+        if( a && b ){ RES.pb('?'); res =true ;RES.ppb();}
+        else if( a || b) {RES.pb(a ? '+' : '-'); res = true;RES.ppb();}
+        else res = false;
+        memo[i][sum + sumaMax] = res; //-1 indef, 1 true : 0 false
+        return res;
     }
     
 }
@@ -22,15 +34,18 @@ bool f(int i,int sum){
 int main(){
     cin>>m;
     forn(i,m){
-        cin>>N>>W;libro.rz(N);//Acum.rz(N+1,0);
-        forn(i,N) cin >> libro[i] / 100;
+        cin >> N >> W; W = W/100; libro.rz(N);//Acum.rz(N+1,0);
+        forn(i,N) {int x;cin >> x;libro[i] = x/100;}
         //Vemos el rango
         sumaMax = 0 ;
         forn(i,N) sumaMax += libro[i];
-        RANGO = 2*sumaMax+1; memo.rz(N+1,vector<int>(RANGO,-1));
+        RANGO = 2*sumaMax+1; memo.rz(N,vector<int>(RANGO,-1));
+        //DBG(memo);
+
         //dfor(i,N-1) Acum[i] = libro[i] + Acum[i+1];
         f(N-1,W);
         DBG(RES);
+        memo.clear(); libro.clear();RES.clear();
     }
 	return 0;
 }
