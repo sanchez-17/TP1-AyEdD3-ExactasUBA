bb79b24008648af15983c564830c8dc50f540757
Sanchez Gaston <sanchez-17@outlook.com>
2023-09-11 17:47:33 -0300

    Ej.1 sin loops pero WA

--- TP1 Tecnicas Algoritmicas/Ej.1 Sendero/sendero_con_numeritos.cpp
+++ TP1 Tecnicas Algoritmicas/Ej.1 Sendero/sendero_con_numeritos.cpp
@@ -4,10 +4,7 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
 #define forr(i,a,b) for(int i = (int) a; i < (int) b; i++)
 #define forn(i,n) forr(i,0,n)
-#define rz resize
-#define sz(x) (int)(x).size()
 #define DBG4(x,y,z,w) cerr << #x << " = " << (x) << " , " << #y << " = " << (y) << " , " << #z << " = " << (z)<< " , " << #w << " = " << (w) << endl;
-
 
 int N,M;
 const int MAX_N = 121, MAX_M = 121;
@@ -23,17 +20,30 @@ bool posible;
  *Derecha = 4
  */
 
+bool todaFilaRecorrida(int fila) {
+    forn(j, M) if (!camino[fila][j]) return false;
+    return true;
+}
+
+bool todaColRecorrida(int col) {
+    forn(i, M) if (!camino[i][col]) return false;
+    return true;
+}
+
 void senderos(int i, int j, int desde, int num){
+
     if(i < 0 || N <= i || N <= j ||j < 0)           return;
     if(camino[i][j]==true || tablero[i][j] == '#')  return;
     if(i == N - 1 && j == M - 1){
         MAX = max(MAX, num);
         MIN = min(MIN, num);
         posible = true;
+        return;
     }else{
-        camino[i][j] = true;
-        if( (j==0 || j==M-1) && (i!=0)) camino[i-1][j] = true;
-        if( (i==0 || i==N-1) && (j!=0)) camino[i][j-1] = true;
+        camino[i][j] = true;bool a,b;
+        a = b = false;
+        if( (j==0 || j==M-1) && (i!=0) && desde != 1 && todaFilaRecorrida(i) && !camino[i-1][j]) {camino[i-1][j] = true;a = true;}
+        if( (i==0 || i==N-1) && (j!=0) && desde != 3 && todaColRecorrida(j) && !camino[i][j-1]) {camino[i][j-1] = true;b = true;}
         if(tablero[i][j] == 'I'){
             if(desde == 1) senderos(i+1, j, 1, num+1);
             if(desde == 2) senderos(i-1, j, 2, num+1);
@@ -51,21 +61,25 @@ void senderos(int i, int j, int desde, int num){
             }
         }
         if(tablero[i][j] == '+'){
-			DBG4(i,j,desde,num);
 			senderos(i+1, j, 1, num+1);
             senderos(i, j+1, 3, num+1);
             senderos(i-1, j, 2, num+1);
             senderos(i, j-1, 4, num+1);
         }
         camino[i][j] = false;
-        if( (j==0 || j==M-1) && (i!=0) && desde != 1) camino[i-1][j]=false;
-        if( (i==0 || i==N-1) && (j!=0) && desde != 3) camino[i][j-1]=false;
+        if(a) camino[i-1][j]=false;
+        if(b) camino[i][j-1]=false;
     }
 }
 
 void solve(){
-	senderos(0,0,1,0);
-
+    if(tablero[N-1][M-1] == '#'){
+        cout<< "IMPOSIBLE" << endl;
+        return;
+    }
+    senderos(0,0,1,0);
+    if(tablero[0][0] != '+' ) senderos(0,0,3,0);
+    
     if(posible) cout << "POSIBLE " << MIN << " " << MAX << endl;
     else cout << "IMPOSIBLE" << endl;
 }
