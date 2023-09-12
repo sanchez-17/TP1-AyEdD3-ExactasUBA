c84de38cc51fc289a526f08faf0a744ab7881081
Sanchez Gaston <sanchez-17@outlook.com>
2023-09-12 03:14:14 -0300

    Ej.3 Dp con poda de suma acumulada

--- TP1 Tecnicas Algoritmicas/Ej.3 Saldos Sospechosos/saldos_con_arrays.cpp
+++ TP1 Tecnicas Algoritmicas/Ej.3 Saldos Sospechosos/saldos_con_arrays.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
 #define forr(i,a,b) for(int i = (int) a; i < (int) b; i++)
 #define forn(i,n) forr(i,0,n)
+#define dforr(i,a,b) for(int i = (int) b-1; i >= (int) a; i--)
+#define dforn(i,n) dforr(i,0,n)
 #define rz resize
 #define sz(x) (int)(x).size()
 
@@ -17,6 +19,7 @@ string RES;
 const int MAX_W = 1000, MAX_N = 100;
 int libro[100];
 bool memo[MAX_N][MAX_N * MAX_W][2] = {{{false}}};
+vector<int> acum;
 
 void calcularRes(string& res){
 	forn(i, sz(res)) if(res[i] != RES[i]) RES[i] = '?';
@@ -33,11 +36,13 @@ bool f(int i, int sum, string& res ){
 		if(memo[i][sum + sumaMax][0]) if (!memo[i][sum + sumaMax][1]) return false;
 	//Pruebo restando
         res.pb('-');
-        bool a = f(i+1, sum-libro[i], res);
+        bool a = false;
+        if(sum - libro[i] - acum[i+1] <= W) a = f(i+1, sum-libro[i], res);
         res.ppb();
 	//Pruebo sumando
         res.pb('+');
-        bool b = f(i+1, sum+libro[i], res);
+        bool b = false;
+        if( sum + libro[i] + acum[i+1] >= W) b = f(i+1, sum+libro[i], res);
         res.ppb();
 	
 		bool r =  a || b;
@@ -54,7 +59,10 @@ int main(){
     forn(c,m){
         cin >> N >> W; W = W/100;
         forn(i,N) {int x; cin >> x; libro[i] = x/100;}
-        //Vemos el rango
+        //Vemos el rango de valores que toma sum en f y la suma acumulada 
+        //de las entradas del libro 
+        acum.rz(N+1,0);
+        dforn(i,N) acum[i] = acum[i+1] + libro[i];
         sumaMax = 0 ;
         forn(i,N) sumaMax += libro[i];
         //Computamos respuesta
@@ -66,7 +74,7 @@ int main(){
 			memo[i][j][0] = false;
 			memo[i][j][1] = false;
 		};
-		RES = "";
+		RES = "";acum.clear();
     }
 	return 0;
 }
