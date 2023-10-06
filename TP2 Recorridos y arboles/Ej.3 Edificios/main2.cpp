ae87b9f4014de4dde7d5fddb5f630cf760bc39dd
sil <silu204@gmail.com>
2023-10-06 20:37:50 -0300

    subo esqueleto del algortimo

--- TP2 Recorridos y arboles/Ej.3 Edificios/main2.cpp
+++ TP2 Recorridos y arboles/Ej.3 Edificios/main2.cpp
@@ -1,13 +1,17 @@
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 #include <utility>
 #include <algorithm>
+#define dd double double
 using namespace std;
 int N, M;
+int contador;
+vector<tuple<int,int,int>> aristasKruskal;
+vector<Aristas> aristas;
+int D, R, Cmax;
 
-#include <bits/stdc++.h>
-
-using namespace std;
+//struct aristas
 
 struct DSU {
     DSU(int n){
@@ -72,10 +76,42 @@ int main(){
 
     return 0;
 }
+
 */
+// para cada c
+// vamos guardando/actualizando las aristas con el nuevo "peso"
+// las ordenamos
+// correr kruskal
+// kruskal devuelve la suma de esas aristas
+// si la suma queda por debajo de 0, guardo esta Cmax y busco otra mejor (por arriba)
+// si la suma queda mayor a 0, no guardo este c y busco otro (por debajo)
+// queremos contar cuantas "iteraciones" (busquedas binarias?) hace.
+// si ya hizo, por ejemplo, 20-40, parar y devolvemos la D y R correspondientes al ultimo Cmax encontrado
+
+void busqueda_binaria(dd a, dd b){
+    while(b-a>1 && contador < 40){
+        contador++;
+        dd mid = ((a+b)/2);
+        //con las aristas creamos nuevas para el kruskal (usa vector tripla)
+        aristasKruskal.clear(); //borro las que ya estaban
+        for(Arista a : aristas){
+            aristasKruskal.push_back({a.u, a.v, mid*a.r - a.d}); //vertice u, vertice v, nuevo peso
+        }
+        if(kruskal(aristasKruskal) < 0){
+            a = mid;
+            Cmax = mid; //el mejor c hasta ahora
+            //actualizar D y R
+            // D = 
+            // R =
+        }else{
+            b=mid;
+        }
+    }
+}
 
 void solve(){
-
+    busqueda_binaria(0, 1e11);
+    cout<<D<<" "<<R<<endl;
 }
 
 int main() {
@@ -87,6 +123,8 @@ int main() {
             int u, v, d, r;
             cin >> u >> v >> d >> r;
         }
+        contador = 0;
+        aristasKruskal.clear(); //las aristas para el kruskal con la suma
         solve();
     }
     return 0;
