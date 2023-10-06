ed0cdc32e975809eb7e293aa67c34d5a13eba947
Gaston Sanchez <sanchez-17@outlook.com>
2023-10-06 18:19:42 -0300

    TP2.Ej.1 Done

--- TP2 Recorridos y arboles/Ej. 1 Malvinas/main2.cpp
+++ TP2 Recorridos y arboles/Ej. 1 Malvinas/main2.cpp
@@ -2,15 +2,15 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
-#define UNVISITED -1
-#define VISITED 1
+#define NO_VISITADO -1
 #define arista pair<int, int>
 
 using namespace std;
 vector<int> dfs_low, dfs_num, padre;
+arista aristaIgnorada;
 vector<arista> claves; //Almacena las aristas clave
-vector<vector<int>> AdjList;
-int C, M, N, verticeAIgnorar, contDFS, raiz, hijosDeRaiz, cantAristasPuente;
+vector<vector<int> > ady;
+int C, M, N, contDFS, cantAristasPuente;
 
 /* Definiciones:
  *  G=(V, E) es Sistema Fragil si existe arista(puente) vw tq G' = (V, E - {vw}) no es conexo.
@@ -32,69 +32,56 @@ int C, M, N, verticeAIgnorar, contDFS, raiz, hijosDeRaiz, cantAristasPuente;
  * Ordenamos S  -> O(M log M)
  */
 
-void dfs(int u) {
+void dfs(int u) { //Visto en una referencia de la practica
     dfs_low[u] = dfs_num[u] = contDFS++; // dfs_low[u] <= dfs_num[u]
-    for(int v : AdjList[u]){
-        if((v == verticeAIgnorar && u == raiz) || (v == raiz && u == verticeAIgnorar)) continue;
-        if (dfs_num[v] == UNVISITED){ // a tree edge
+    for(int v : ady[u]){
+        arista e = {min(u, v), max(u, v)};
+        if(e == aristaIgnorada) continue;
+        if (dfs_num[v] == NO_VISITADO){ // Arista del arbol DFS
             padre[v] = u;
-            if (u == raiz) hijosDeRaiz++; // special case if u is a root
             dfs(v);
-            if (dfs_low[v] > dfs_num[u]) // for bridge
+            if (dfs_low[v] > dfs_num[u]) //Condicion para saber si una arista es puente
                 cantAristasPuente++;
-            dfs_low[u] = min(dfs_low[u], dfs_low[v]); // update dfs_low[u]
+            dfs_low[u] = min(dfs_low[u], dfs_low[v]);
         }
-        else if (v != padre[u]) // a back edge and not direct cycle
-            dfs_low[u] = min(dfs_low[u], dfs_num[v]); // update dfs_low[u]
+        else if (v != padre[u]) // Tenemos backEdge
+            dfs_low[u] = min(dfs_low[u], dfs_num[v]);
     }
 }
 
 void solve(){
-    //Por cada arista en E
-
     for(int u = 0; u < N; u++) {
-        for (int v: AdjList[u]) {
-            cantAristasPuente = 0;
-            contDFS = 0;
-            hijosDeRaiz = 0;
-            dfs_low = vector<int>(N);
-            dfs_num = vector<int>(N);
-            raiz = u;
+        for (int v: ady[u]) {
+            aristaIgnorada = {min(u, v), max(u, v)};
+            cantAristasPuente = contDFS = 0;
+            dfs_low = vector<int>(N, 0);
+            dfs_num = vector<int>(N, NO_VISITADO);
             padre = vector<int>(N, -1);
-            verticeAIgnorar = v;
             dfs(u);
             if (cantAristasPuente != 0) {
-                arista clave = {min(u, v), max(u, v)};
-                claves.push_back(clave);
+                claves.push_back(aristaIgnorada);
             }
-
         }
     }
     sort(claves.begin(), claves.end());
-    cout << claves.size() << endl;
-    if(claves.size()!=0){
-        cout << claves[0].first << claves[0].second << endl;
-    }
-    for(int i = 1; i < claves.size();i++ ){
-        if(claves[i-1]==claves[i]){
-            continue;
-        }
-        cout << claves[i].first << claves[i].second << endl;
-    }
+    int cant = claves.size();
+    cout << cant/2 << "\n";
+    for(int i = 0; i < cant; i = i + 2) cout << claves[i].first << " "<< claves[i].second << "\n";
 }
 
 int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit); //Optimiza I/O de datos
     cin >> C;
     for(int c = 0; c < C; c++){
         cin >> N >> M;
-        AdjList = vector<vector<int>>(N);
+        ady = vector<vector<int>>(N);
         claves.clear();
         for(int i = 0; i < M; i++){
             //Armamos el grafo
             int v, w;
             cin >> v >> w;
-            AdjList[v].push_back(w);
-            AdjList[w].push_back(v);
+            ady[v].push_back(w);
+            ady[w].push_back(v);
         }
         solve();
     }
