f4a7b2dfea2f3982cb1524a0d969a01ed350a903
sil <silu204@gmail.com>
2023-10-06 19:34:17 -0300

    entrada de datos

---
+++ TP2 Recorridos y arboles/Ej.3 Edificios/main2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+int N, M;
+
+void solve(){
+    
+}
+
+int main() {
+    int C;
+    cin >> C;
+    for(int c = 0; c < C; c++){
+        cin >> N >> M;
+        for(int m = 0; m < M; m++){
+            int u, v, d, r;
+            cin >> u >> v >> d >> r;
+        }
+        solve();
+    }
+    return 0;
+}
+
