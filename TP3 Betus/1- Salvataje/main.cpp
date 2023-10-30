ac3d746f101c5650a8f5012b7b59a0cc8b72f5d2
sil <silu204@gmail.com>
2023-10-30 16:58:01 -0300

    TP3.Ej1. input

--- TP3 Betus/1- Salvataje/main.cpp
+++ TP3 Betus/1- Salvataje/main.cpp
@@ -17,27 +17,31 @@
 #include <string>
 using namespace std;
 int N, M;
-vector<pair<int,int>> aristas;
+vector<vector<pair<int,int>>> aristas; // Vecino y peso
 
 int main(){
     int c;
     cin>>c;
     for (int i = 0; i < c; ++i) {
-        cin>>N>>M;
-        aristas = vector<pair<int,int>>(N*4); //por el vertice fantasma
-        for (int j = 0; j < M; ++j) {
+        cin >> N >> M;
+        aristas = vector<vector<pair<int,int>>>(N*4 + 2); // Grafo triplicado + los vertice dummy
+        for (int j = 0; j < M; ++j) { // Indices de pasadizos
             int u, v;
             cin>>u>>v;
-            
-            for (int k = 0; k < 3; ++k) {
-                aristas[u + 4*k] = {v+4*(k+1),2}; //vecina y peso
-            }
-            //luego todas las demas tienen peso 1
-            for (int w = 0; w < N; ++w) {
-                for(int k = 0; k < 3; ++k)
-                    aristas[w] = {w+1,1};
+
+            for (int k = 0; k < 3; ++k) { // Conecta cada salon con su pasadizo. k <4??
+                aristas[u + 4*k].push_back({v+4*(k+1),2}); // Peso = 2min
             }
 
+            for (int w = 0; w < N; ++w) { // Conecta cada salon con su siguiente
+                for(int k = 0; k < 3; ++k)
+                    aristas[w].push_back({w+1,1}); // Peso = 1min
+            }
+        }
+        //Conectamos los vertices dummy
+        aristas[N*4].push_back({0, 1});
+        for(int k = 0; k < 4; ++k){
+            //aristas[N*4*k -1]
         }
     }
 
