aa13abed77a2172ed4c63e6895fcf418267084d3
Gaston Sanchez <sanchez-17@outlook.com>
2023-11-03 15:00:02 -0300

    Update main3.cpp

--- TP3 Betus/4- Envios/main3.cpp
+++ TP3 Betus/4- Envios/main3.cpp
@@ -12,7 +12,7 @@ struct Arista{
     }
     int v, c;
 };
-vector<vector<Arista>> ady;
+vector<vector<Arista> > ady,adyAux;
 
 /* Cada calle es una arista de capacidad c(e)
  * Dado un valor C, la cantidad max. de herramientas que puede llevar cada persona es div_entera(c(e), C).
@@ -31,16 +31,96 @@ vector<vector<Arista>> ady;
  *
  */
 
- //version con busqueda binaria no dependiente de cant de iteraciones
+//version con busqueda binaria no dependiente de cant de iteraciones
 
- void actualizarPesosEnG(int c){
-     //Actualizo las capacidades de las aristas a c(e)/C
-     for(int i = 0; i < N; i++)
-         for (Arista& e: ady[i]) {
-             capacidades[i][e.v] = e.c / c;
-             capacidades[e.v][i] = 0;
-         }
- }
+void actualizarPesosEnG(int c){
+    //Actualizo las capacidades de las aristas a c(e)/C
+    for(int i = 0; i < N; i++)
+        for (Arista& e: adyAux[i]) {
+            capacidades[i][e.v] = e.c / c;
+            capacidades[e.v][i] = 0;
+        }
+}
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int u = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (auto next : ady[u]) { //cur->next
+            int v = next.v;
+            if (parent[v] == -1 && capacidades[u][v]) {
+                parent[v] = u;
+                int new_flow = min(flow, capacidades[u][v]);
+                if (v == t)
+                    return new_flow;
+                q.push({v, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int EyK(int s, int t){
+    int flow = 0;
+    vector<int> parent(N);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int v = t;
+        while (v != s) {
+            int u = parent[v];
+            capacidades[u][v] -= new_f#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int t, N, M, X;
+const int INF = 1e9;
+vector<vector<int>> capacidades;
+struct Arista{
+    Arista(int _v, int _c){
+        v = _v; c = _c;
+    }
+    int v, c;
+};
+vector<vector<Arista>> ady,adyAux;
+
+/* Cada calle es una arista de capacidad c(e)
+ * Dado un valor C, la cantidad max. de herramientas que puede llevar cada persona es div_entera(c(e), C).
+ * Ejemplo: para un calle e, c(e) = 13. C = 5. div_entera(13,2) = 2
+ * Es decir, fijamos un valor C. Y modelamos las aristas como div_entera(c(e), C) para toda arista e en G.
+ * Luego calculamos flujo maximo. Hacemos busqueda binaria de C con la siguiente propiedad binaria:
+ * "Si x personas pueden transportar C objetos por G hasta t, pueden transportar C-1"
+ * Entrada:
+ *  N : Cant de esquinas     1 <= N <= 1e2
+ *  M : Cant de calles e     1 <= M <= 1e3
+ *  x : Cant de personas     1 <= x <= 1e6
+ *  Luego M lineas de calle e = v, w, c donde:   //Las calles tienen direccion
+ *     v : esquina de origen
+ *     w : esquina de destino
+ *     c : capacidad de la calle   1 <= c <= 1e6
+ *
+ */
+
+//version con busqueda binaria no dependiente de cant de iteraciones
+
+void actualizarPesosEnG(int c){
+    //Actualizo las capacidades de las aristas a c(e)/C
+    for(int i = 0; i < N; i++)
+        for (Arista& e: adyAux[i]) {
+            capacidades[i][e.v] = e.c / c;
+            capacidades[e.v][i] = 0;
+        }
+}
 
 int bfs(int s, int t, vector<int>& parent) {
     fill(parent.begin(), parent.end(), -1);
@@ -88,14 +168,14 @@ int EyK(int s, int t){
 }
 
 int busqueda_binaria(int a, int b){
-     int C;
+    int C;
     while(b - a > 1){ //P(a) verdadero
         C =(a+b)/2;
         //Actualizo las capacidades de las aristas a c(e)/C
         actualizarPesosEnG(C);
         int flow = EyK(0,N-1);
         actualizarPesosEnG(1);
-        if (flow < X)//P(C) falso: "Si x personas pueden transportar C objetos por G hasta t, pueden transportar C-1"
+        if (flow < X) //P(C) falso: "Si x personas pueden transportar C objetos por G hasta t, pueden transportar C-1"
             b = C;
         else
             a = C;
@@ -103,11 +183,11 @@ int busqueda_binaria(int a, int b){
     return a; //El maximo valor de C que cumple P(C) es a
 }
 
-int solve(int cota){
+int solve(){
     int flow = EyK(0,N-1), res;
     //Restauramos los valores iniciales de las capacidades
     actualizarPesosEnG(1);
-    if(flow >= X) res = busqueda_binaria(0, 2*cota);
+    if(flow >= X) res = busqueda_binaria(0, flow);
     else res = 0;
     return X * res;
 }
@@ -118,22 +198,79 @@ int main(){
         cin >> N >> M >> X;
         capacidades = vector<vector<int>>(N, vector<int>(N, 0));
         ady = vector<vector<Arista>>(N);
-        int maxPeso = 0;
+        adyAux = vector<vector<Arista>>(N);
         for(int i = 0; i < M; i++){
             int v, w, c;
             cin >> v >> w >> c;
-            maxPeso = max(maxPeso, c);  //cota superior de la busqueda binaria
             v--; w--;
             Arista vw(w , c);
-            //Arista wv(v, c);
+            Arista wv(v, c);
             ady[v].push_back(vw);
-            //ady[w].push_back(wv);
+            ady[w].push_back(wv);
+            adyAux[v].push_back(vw);
             capacidades[v][w] = c;
         }
-        cout << solve(maxPeso) << endl;
+        cout << solve() << endl;
         t--;
     }
     return 0;
 }
 
+low;
+            capacidades[v][u] += new_flow;
+            v = u;
+        }
+    }
+
+    return flow;
+}
+
+int busqueda_binaria(int a, int b){
+    int C;
+    while(b - a > 1){ //P(a) verdadero
+        C =(a+b)/2;
+        //Actualizo las capacidades de las aristas a c(e)/C
+        actualizarPesosEnG(C);
+        int flow = EyK(0,N-1);
+        actualizarPesosEnG(1);
+        if (flow < X) //P(C) falso: "Si x personas pueden transportar C objetos por G hasta t, pueden transportar C-1"
+            b = C;
+        else
+            a = C;
+    }
+    return a; //El maximo valor de C que cumple P(C) es a
+}
+
+int solve(){
+    int flow = EyK(0,N-1), res;
+    //Restauramos los valores iniciales de las capacidades
+    actualizarPesosEnG(1);
+    if(flow >= X) res = busqueda_binaria(0, flow);
+    else res = 0;
+    return X * res;
+}
+
+int main(){
+    cin >> t;
+    while(t>0){
+        cin >> N >> M >> X;
+        capacidades = vector<vector<int>>(N, vector<int>(N, 0));
+        ady = vector<vector<Arista>>(N);
+        adyAux = vector<vector<Arista>>(N);
+        for(int i = 0; i < M; i++){
+            int v, w, c;
+            cin >> v >> w >> c;
+            v--; w--;
+            Arista vw(w , c);
+            Arista wv(v, c);
+            ady[v].push_back(vw);
+            ady[w].push_back(wv);
+            adyAux[v].push_back(vw);
+            capacidades[v][w] = c;
+        }
+        cout << solve() << endl;
+        t--;
+    }
+    return 0;
+}
 
